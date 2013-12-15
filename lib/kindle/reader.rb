3b96a35b718cb03c41df298fb7158cb2af3c515c
Darren Cauthon <darren@cauthon.com>
2013-12-15 11:09:05 -0600

    Move the state into a parameter that is passed between the methods.

--- lib/kindle/reader.rb
+++ lib/kindle/reader.rb
@@ -6,47 +6,38 @@ module Kindle
     
     KINDLE_URL = 'http://kindle.amazon.com'
 
-    attr_reader :state
-
     def initialize(options = {:login => nil, :password => nil})
-      @state = {}
-      state[:current_offset] = 25
-      state[:current_upcoming] = []
       options.each_pair { |k,v| instance_variable_set("@#{k}", v) }
-      agent = Mechanize.new
-      agent.redirect_ok = true
-      agent.user_agent_alias = 'Windows IE 7'
-      state[:agent] = agent
     end
 
-    def get_login_page
+    def get_login_page state
       page = state[:agent].get(KINDLE_URL)
       page.link_with(:text => "Sign in").click
     end
 
-    def login
-      login_page = get_login_page
+    def login state
+      login_page = get_login_page state
       login_page.forms.first.email    = @login
       login_page.forms.first.password = @password
       page = login_page.forms.first.submit
       page.forms.first.submit
     end
 
-    def fetch_highlights page
+    def fetch_highlights page, state
       state[:asins] = []
       page = page.link_with(:text => 'Your Highlights').click
-      new_highlights = extract_highlights(page)
+      new_highlights = extract_highlights(page, state)
       highlights = []
       until new_highlights.length == 0 do
         highlights << new_highlights
-        results = next_highlights
+        results = next_highlights state
         page = results[:page]
-        new_highlights = extract_highlights(page)
+        new_highlights = extract_highlights(page, state)
       end
       highlights.flatten
     end
 
-    def extract_highlights(page)
+    def extract_highlights(page, state)
       hls = (page/".yourHighlight")
       asins = (page/".asin").collect{|asin| asin.text}
       highlights = []
@@ -56,7 +47,7 @@ module Kindle
         state[:author] = (page/".yourHighlightsHeader .author").text.to_s.strip
         state[:current_offset] = ((page/".yourHighlightsHeader").collect{|h| h.attributes['id'].value }).first.split('_').last
         (page/".yourHighlight").each do |hl|
-          highlight = parse_highlight(hl)
+          highlight = parse_highlight(hl, state)
           highlights << highlight
           if !state[:asins].include?(highlight.asin)
             state[:asins] << highlight.asin unless state[:asins].include?(highlight.asin)
@@ -66,26 +57,35 @@ module Kindle
       highlights
     end
 
-    def next_highlights
+    def next_highlights state
       asins_string = state[:asins].map{|l| "used_asins[]=#{l}" } * '&'
       upcoming_string = state[:current_upcoming].map{|l| "upcoming_asins[]=#{l}" } * '&'
       current_offset = state[:current_offset]
       url = "https://kindle.amazon.com/your_highlights/next_book?#{asins_string}&current_offset=#{state[:current_offset]}&#{upcoming_string}"
       ajax_headers = { 'X-Requested-With' => 'XMLHttpRequest', 'Host' => 'kindle.amazon.com' }
       page = state[:agent].get(url,[],'https://kindle.amazon.com/your_highlight', ajax_headers)
-      highlights = extract_highlights page
+      highlights = extract_highlights page, state
       { page: page, highlights: highlights }
     end
 
-    def parse_highlight(hl)
+    def parse_highlight(hl, state)
       highlight = (hl/".highlight").text
       asin      = (hl/".asin").text
       Highlight.new(highlight, asin, state[:title], state[:author])
     end
 
     def get_kindle_highlights
-      page = login
-      fetch_highlights page
+
+      state = {}
+      state[:current_offset] = 25
+      state[:current_upcoming] = []
+      agent = Mechanize.new
+      agent.redirect_ok = true
+      agent.user_agent_alias = 'Windows IE 7'
+      state[:agent] = agent
+
+      page = login state
+      fetch_highlights page, state
     end
 
   end
